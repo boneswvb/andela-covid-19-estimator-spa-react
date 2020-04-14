75e661464351e0a8e1aebcacaa808a6e04ec9786
Wim von Benecke <boneswvb@gmail.com>
2020-04-14 18:05:05 +0200

    Updates

---
+++ precache-manifest.f347c922a1c02ab2ccabd00c7965cbae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b009bc112401e63011d3bb9b90bbdea",
+    "url": "/andela-covid-19-estimator-spa-react/index.html"
+  },
+  {
+    "revision": "578cbaa42931060ba38c",
+    "url": "/andela-covid-19-estimator-spa-react/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "25911b697d9a07672ffd",
+    "url": "/andela-covid-19-estimator-spa-react/static/css/main.5f57218f.chunk.css"
+  },
+  {
+    "revision": "578cbaa42931060ba38c",
+    "url": "/andela-covid-19-estimator-spa-react/static/js/2.f63eec51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/andela-covid-19-estimator-spa-react/static/js/2.f63eec51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25911b697d9a07672ffd",
+    "url": "/andela-covid-19-estimator-spa-react/static/js/main.17f52835.chunk.js"
+  },
+  {
+    "revision": "fd91adee9ddc53210d25",
+    "url": "/andela-covid-19-estimator-spa-react/static/js/runtime-main.3d6fcddd.js"
+  },
+  {
+    "revision": "79d5114fe9ed670126860ee0be7e17e7",
+    "url": "/andela-covid-19-estimator-spa-react/static/media/wim.79d5114f.jpg"
+  }
+]);
